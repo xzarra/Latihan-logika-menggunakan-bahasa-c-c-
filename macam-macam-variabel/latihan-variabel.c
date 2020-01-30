ecc40d641db43e23d09c727d6727b7740423dd56
xzarra <aryasatya6969@gmail.com>
2020-01-30 15:58:50 +0700

    Macam macam variabel dalam bahasa c

---
+++ macam-macam-variabel/latihan-variabel.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+    int a;
+    char b;
+    float c;
+
+    a = 20;
+    b = "A";
+    c = 1.337;
+
+    printf("%i\n",a);
+    printf("%c\n",b);
+    printf("%f\n",c);
+
+    return 0;
+}
