69ffe4207c0f11274428702e90325cabd3df2298
xzarra <aryasatya6969@gmail.com>
2020-01-30 16:18:59 +0700

    Latihan Sequence

--- macam-macam-variabel/latihan-variabel.c
+++ macam-macam-variabel/latihan-variabel.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main (){
-    int a;
-    char b;
-    float c;
+    int a; // integer adalah tipe data yang berguna untuk menampung angka yang bulat
+    char b; // character adalah tipe data yang berguna untuk menambahkan 1 huruf atau 1 karakter
+    float c; // float adalah tipe data yang berguna untuk menampung angka pecahan
 
-    a = 20;
+    a = 20; 
     b = "A";
     c = 1.337;
 
-    printf("%i\n",a);
-    printf("%c\n",b);
-    printf("%f\n",c);
+    printf("%i\n",a);//inisialisasi integer bisa %i atau %d
+    printf("%c\n",b);//inisialisasi character adalah %c
+    printf("%f\n",c);//inisialisasi float adalah %f
 
     return 0;
 }
---
+++ sequence/sequence.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+sequence adalah suatu rangkaian pengerjaan program
+yang dilakukan berurutan.
+
+jika ingin menambahkan spasi bisa dengan printf(" ");
+lalu jika ingin menambahkan bintang bisa dengan printf("*");
+jika ingin membuat baris baru (newline) bisa dengan printf("\n");
+
+kita latihan untuk membuat grafik seperti :
+              *
+            /
+    *     *
+  /  \  /
+*     *
+*/
+int main (){
+    printf(" ");printf(" ");printf(" ");printf(" ");printf(" ");printf(" ");printf(" ");printf(" ");printf("*");printf("\n");
+    printf(" ");printf(" ");printf("*");printf(" ");printf(" ");printf(" ");printf("*");printf(" ");printf(" ");printf("\n");
+    printf("*");printf(" ");printf(" ");printf(" ");printf("*");printf(" ");printf(" ");printf(" ");printf(" ");printf("\n");
+    return 0;
+}
