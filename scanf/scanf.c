b57c977994795355a506c0bd983c6dad05c2116f
xzarra <aryasatya6969@gmail.com>
2020-01-30 15:42:42 +0700

    Fungsi Scanf

---
+++ scanf/scanf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (){
+    int bilangan; //inisialisasi bilangan
+    printf("masukkan bilangan yang akan ditampilkan dibawah = ");
+    scanf("%i", &bilangan); //%i adalah variabel integer
+    printf("bilangan yang tampil = %i",bilangan);//bilangan yang akan tampil 
+}
