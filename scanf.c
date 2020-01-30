dfc82d2070de2c60c14163d0a30feecb159b3e49
xzarra <aryasatya6969@gmail.com>
2020-01-30 15:41:33 +0700

    Fungsi Printf

--- scanf.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main (){
-    int bilangan; //inisialisasi bilangan
-    printf("masukkan bilangan yang akan ditampilkan dibawah = ");
-    scanf("%i", &bilangan); //%i adalah variabel integer
-    printf("bilangan yang tampil = %i",bilangan);//bilangan yang akan tampil 
-}
